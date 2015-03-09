d879178cfbfec6921b1c79f7cd359b9f04f34ae1
Masayoshi Mizutani <mizutani@sfc.wide.ad.jp>
2015-03-09 20:02:24 +0900

    split index page and flow viewer

--- viewer/app.js
+++ viewer/app.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes_index = require('./routes/index');
+var routes_flow = require('./routes/flow');
 
 var app = express();
 
@@ -20,7 +21,8 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', routes_index);
+app.use('/flow', routes_flow);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
